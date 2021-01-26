f1e2b63728925972224f526ab04aff257c69d0b9
Pawel Szambelan <p.szambelan1@gmail.com>
2021-01-26 19:37:31 +0100

    first test :-)

--- src/app/model/Rook.ts
+++ src/app/model/Rook.ts
@@ -2,7 +2,7 @@ import { Board } from './Board';
 import { BOARDSIZE } from './Constances';
 import { Piece } from './Piece';
 import { PieceMovement } from './PieceMovement';
-import { Column, Row, Side, Square } from './Types';
+import { Column, columns, Row, Side, Square } from './Types';
 
 export class Rook extends Piece implements PieceMovement {
   constructor(id: string, side: Side) {
@@ -22,12 +22,11 @@ export class Rook extends Piece implements PieceMovement {
     const movesToGo: Square[] = [];
 
     for (let i = position.row + 1; i <= BOARDSIZE; i++) {
-      if (board.onPositionPiece({ column: position.column, row: i as Row } as Square) === null) {
-        // is 'as Square' needed here? without it it also works! but it's less visible
-        movesToGo.push({ column: position.column, row: i as Row } as Square); // same here
+      if (board.onPositionPiece({ column: position.column, row: i as Row }) === null) {
+        movesToGo.push({ column: position.column, row: i as Row });
       } else {
         if (this.checkIfOponent(position.column, i as Row, board)) {
-          movesToGo.push({ column: position.column, row: i as Row } as Square); // same here
+          movesToGo.push({ column: position.column, row: i as Row });
           break;
         } else break;
       }
@@ -57,11 +56,11 @@ export class Rook extends Piece implements PieceMovement {
     const movesToGo: Square[] = [];
 
     for (let i = position.column.indexOf(position.column) - 1; i >= 0; i--) {
-      if (board.onPositionPiece({ column: position.column[i] as Column, row: position.row }) === null) {
-        movesToGo.push({ column: position.column[i] as Column, row: position.row });
+      if (board.onPositionPiece({ column: columns[i], row: position.row }) === null) {
+        movesToGo.push({ column: columns[i], row: position.row });
       } else {
-        if (this.checkIfOponent(position.column[i] as Column, position.row, board)) {
-          movesToGo.push({ column: position.column[i] as Column, row: position.row });
+        if (this.checkIfOponent(columns[i], position.row, board)) {
+          movesToGo.push({ column: columns[i], row: position.row });
           break;
         } else break;
       }
@@ -74,11 +73,11 @@ export class Rook extends Piece implements PieceMovement {
     const movesToGo: Square[] = [];
 
     for (let i = position.column.indexOf(position.column) + 1; i < BOARDSIZE; i++) {
-      if (board.onPositionPiece({ column: position.column[i] as Column, row: position.row }) === null) {
-        movesToGo.push({ column: position.column[i] as Column, row: position.row });
+      if (board.onPositionPiece({ column: columns[i], row: position.row }) === null) {
+        movesToGo.push({ column: columns[i], row: position.row });
       } else {
-        if (this.checkIfOponent(position.column[i] as Column, position.row, board)) {
-          movesToGo.push({ column: position.column[i] as Column, row: position.row });
+        if (this.checkIfOponent(columns[i], position.row, board)) {
+          movesToGo.push({ column: columns[i], row: position.row });
           break;
         } else break;
       }
--- test/app/model/Rook.spec.ts
+++ test/app/model/Rook.spec.ts
@@ -1,19 +1,33 @@
 import { Rook } from '../../../src/app/model/Rook';
-import { Square, Row, Column } from '../../../src/app/model/Types';
+import { Square } from '../../../src/app/model/Types';
 import { Board } from '../../../src/app/model/Board';
-import { Piece } from '../../../src/app/model/Piece';
 
 describe('Rook movement', () => {
-  it('Check possibles values, when Rook is on A1', () => {
+  it('Check possible squares to go, when Rook is on A1', () => {
     const mockOnPositionPiece = jest.fn();
     const board: Board = { onPositionPiece: mockOnPositionPiece };
     mockOnPositionPiece.mockReturnValue(null);
-
+    const possibleMovesWhenRookOnA1 = [
+      { column: 'A', row: 2 },
+      { column: 'A', row: 3 },
+      { column: 'A', row: 4 },
+      { column: 'A', row: 5 },
+      { column: 'A', row: 6 },
+      { column: 'A', row: 7 },
+      { column: 'A', row: 8 },
+      { column: 'B', row: 1 },
+      { column: 'C', row: 1 },
+      { column: 'D', row: 1 },
+      { column: 'E', row: 1 },
+      { column: 'F', row: 1 },
+      { column: 'G', row: 1 },
+      { column: 'H', row: 1 },
+    ];
     const rook = new Rook('1', 'WHITE');
     const rockPosition: Square = { column: 'A', row: 1 };
 
     const rockPossibleMoves = rook.possibleMoves(rockPosition, board);
 
-    expect(rockPossibleMoves).toBe('here will go Square[] with possible moves to go');
+    expect(rockPossibleMoves).toMatchObject(possibleMovesWhenRookOnA1);
   });
 });
