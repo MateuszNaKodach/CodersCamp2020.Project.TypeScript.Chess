5bd5d5df34c05e5b5f60a10ff1f393e82759ce66
Pawel Szambelan <p.szambelan1@gmail.com>
2021-01-28 14:44:35 +0100

    #31 initial tests

---
+++ src/app/model/Move.ts
@@ -0,0 +1,7 @@
+import { Board } from './Board';
+import { Piece } from './Piece';
+import { Square } from './Types';
+
+export class Move {
+  movePiece(piece: Piece, squareFrom: Square, squareTo: Square, board: Board): void {}
+}
