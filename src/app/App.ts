83457289b8816743d402ea3eee6fccd7533a088d
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-29 19:35:43 +0100

    #2 Add CSS classes to button, add font import, add outline reset rule, add CSS variables

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,12 +1,21 @@
 import { Button } from './view/Button';
 
 export const App = () => {
+  //Test Buttona na żywym organizmie  vvv
   const button: Button = Button.small('buttonId')
     .withText('Text')
     .onClick(() => console.log('sad'));
 
+  const largeButton: Button = Button.large('largeButton')
+    .withText("I'm large")
+    .onClick(() => console.log('large'));
+
+  const largeButtonHtml = largeButton.toHtml();
+  document.body.appendChild(largeButtonHtml);
+
   const buttonHtml = button.toHtml();
   document.body.appendChild(buttonHtml);
 
-  Button.fromHtml(buttonHtml).large().withText('Sratata').updateHtml();
+  Button.fromHtml(buttonHtml).withText("I'm small").updateHtml();
+  //Koniec testu ^^^
 };
