c9cb9fb117eec95a2febc53cfcc9c54d8c7b4c19
Tomek <tomasz.dworniczak@hotmail.com>
2021-01-30 20:28:46 +0100

    #2 Code refactor

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,27 +2,24 @@ import { Button } from './view/Button';
 
 export const App = () => {
   //Test Buttona na żywym organizmie  vvv
-
-  const onClickFn = () => {
-    console.log('jajjaja');
+  const myOnClickFn = () => {
+    console.log('Nowa funkcja onclick');
   };
   const button: Button = Button.small('buttonId')
     .withText('Text')
-    .onClick(() => console.log('Small'));
+    .onClick(() => console.log('Small, defaultowa funkcja onclick'));
 
   const largeButton: Button = Button.large('largeButton')
     .withText("I'm large")
-    .onClick(() => console.log('large'));
+    .onClick(() => console.log('Large, defaultowa funkcja onclick'));
 
   const largeButtonHtml = largeButton.toHtml();
   document.body.appendChild(largeButtonHtml);
-
-  // console.log(largeButtonHtml);
 
   const buttonHtml = button.toHtml();
   document.body.appendChild(buttonHtml);
 
   //tutaj mały teścik - wywołując fromHtml i później onClick na buttonie okazuje się, że pierwtona funkcja na klik nie zostaje ndapisana, a tylko dopisujemy nową funkcję ale do właściwości onclick elementu
-  Button.fromHtml(buttonHtml).withText("I'm small").large().updateHtml();
+  Button.fromHtml(buttonHtml).withText("I'm small").onClick(myOnClickFn).updateHtml();
   //Koniec testu ^^^
 };
