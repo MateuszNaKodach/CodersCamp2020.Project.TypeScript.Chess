af9128dfba95c5ee117901aaf74997502e138366
PiotrWR <pwrynio@gmail.com>
2021-01-30 11:37:56 +0100

    Add Knight's moves on empty Board

--- src/app/model/Knight.ts
+++ src/app/model/Knight.ts
@@ -1,5 +1,6 @@
 import { Board } from './Board';
 import { Piece } from './Piece';
+import { BOARDSIZE } from './Constances';
 import { PieceMovement } from './PieceMovement';
 import { Column, columns, Row, Side, Square } from './Types';
 
@@ -9,7 +10,43 @@ export class Knight extends Piece implements PieceMovement {
   }
 
   possibleMoves(position: Square, board: Board): Square[] {
-    const movesToGo = [] as Square[];
+    let movesToGo = [] as Square[];
+    // TODO: wypisz wszystkie pola
+    movesToGo = movesToGo.concat(this.movesOnEmptyBoard(position, board));
+    // TODO: usuń pola zajęte
+    return movesToGo;
+  }
+
+  private movesOnEmptyBoard(position: Square, board: Board): Square[] {
+    const movesToGo: Square[] = [];
+    const currentColumnNumber = columns.indexOf(position.column);
+    const currentRowNumber = position.row;
+
+    if (currentColumnNumber + 2 < BOARDSIZE && currentRowNumber + 1 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber + 2], row: (currentRowNumber + 1) as Row });
+    }
+    if (currentColumnNumber + 2 < BOARDSIZE && currentRowNumber - 1 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber + 2], row: (currentRowNumber - 1) as Row });
+    }
+    if (currentColumnNumber + 1 < BOARDSIZE && currentRowNumber + 2 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber + 1], row: (currentRowNumber + 2) as Row });
+    }
+    if (currentColumnNumber + 1 < BOARDSIZE && currentRowNumber - 2 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber + 1], row: (currentRowNumber - 2) as Row });
+    }
+    if (currentColumnNumber - 1 < BOARDSIZE && currentRowNumber + 2 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber - 1], row: (currentRowNumber + 2) as Row });
+    }
+    if (currentColumnNumber - 1 < BOARDSIZE && currentRowNumber - 2 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber - 1], row: (currentRowNumber - 2) as Row });
+    }
+    if (currentColumnNumber - 2 < BOARDSIZE && currentRowNumber + 1 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber - 2], row: (currentRowNumber + 1) as Row });
+    }
+    if (currentColumnNumber - 2 < BOARDSIZE && currentRowNumber - 1 < BOARDSIZE) {
+      movesToGo.push({ column: columns[currentColumnNumber - 2], row: (currentRowNumber - 1) as Row });
+    }
+
     return movesToGo;
   }
 }
