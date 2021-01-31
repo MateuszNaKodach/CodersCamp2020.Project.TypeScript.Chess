8e7076b9a626ed99f63c9903584f4fd315eb4071
Paweł <p.szambelan1@gmail.com>
2021-01-31 20:31:31 +0100

    Model/#31 piece movement (#34)
    * more project settings
    
    * #31 initial tests
    
    * #31 refactoring + ChessBoard class
    
    * #31 implements methods + tests
    
    * #31 remove unnecessary code
    
    * #31 tests fixes
    
    * #31 test fixes 2.0
    
    * #31 tests fixes
    
    * #31 pull develop fix
    
    * #31 imports, types, tests fixes
    
    * #22 Knight possible movements | Back to first Pawn version
    
    * #31 piece movement | Update tests
    
    * #31 piece movement | Update Pawn.ts without errors in tests
    
    * #31 shorten the return value
    
    * #31 piece movement | Fix Pawn.ts
    
    * #31 fix one import
    
    * Update test/app/model/ChessBoard.spec.ts
    
    Co-authored-by: Mateusz Nowak <kontakt.mateusznowak@gmail.com>
    
    Co-authored-by: PiotrWR <pwrynio@gmail.com>
    Co-authored-by: Mateusz Nowak <kontakt.mateusznowak@gmail.com>

---
+++ src/app/model/ChessBoard.ts
@@ -0,0 +1,21 @@
+import { PiecePositions } from './PiecesPositions';
+import { Piece } from './Piece';
+import { SquareWithPiece, Square } from './Types';
+import { PieceMoves } from './PieceMoves';
+
+export class ChessBoard implements PieceMoves, PiecePositions {
+  constructor(private boardWithPieces: SquareWithPiece) {}
+
+  onPositionPiece(square: Square): Piece | undefined {
+    return this.boardWithPieces[`${square.column}${square.row}`];
+  }
+
+  movePiece(squareFrom: Square, squareTo: Square): void {
+    const piece = this.onPositionPiece(squareFrom);
+    if (!piece) {
+      throw new Error(`There is no piece on square!`);
+    }
+    delete this.boardWithPieces[`${squareFrom.column}${squareFrom.row}`];
+    this.boardWithPieces[`${squareTo.column}${squareTo.row}`] = piece;
+  }
+}
