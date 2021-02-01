30a5db08cd2bccc801f6594916ff2932c934c9fa
lamparina <anna.lamperska@gmail.com>
2021-02-01 23:11:40 +0100

    Fix Codacy Static Code Analysis issue

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,6 +1,6 @@
 import { Chessboard } from './view/ChessBoardView';
 
-export const App = () => {
+export const App = (): void => {
   const chessboard: Chessboard = new Chessboard();
   document.body.appendChild(chessboard.createBoard());
 };
