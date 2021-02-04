d7820dd903a0cb100bfb897ea2fd05346cf85406
Pawel Szambelan <p.szambelan1@gmail.com>
2021-02-04 23:38:07 +0100

    #26 rendering pieces on chessboard

--- src/app/App.ts
+++ src/app/App.ts
@@ -1,6 +1,9 @@
 import { Chessboard } from './view/ChessBoardView';
+import { pieceRendering } from './view/PieceRender';
 
 export const App = (): void => {
   const chessboard: Chessboard = new Chessboard();
   document.body.appendChild(chessboard.createBoard());
+
+  pieceRendering();
 };
