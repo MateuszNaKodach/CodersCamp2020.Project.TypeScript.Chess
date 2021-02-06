31425c74a3608fb1a144f456778f8c399312ce99
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-06 21:48:19 +0100

    #32 Attempt to TDD, test draft

---
+++ test/app/model/ChessEngine.spec.ts
@@ -0,0 +1,24 @@
+import { ChessEngine, PieceWasCaptured, PieceWasMoved } from '../../../src/app/model/ChessEngine';
+import { Side, Square, SquareWithPiece } from '../../../src/app/model/Types';
+import { ChessBoard } from '../../../src/app/model/ChessBoard';
+import { Pawn } from '../../../src/app/model/Pawn';
+import { Player } from '../../../src/app/model/Player';
+
+describe('Chess Engine', () => {
+  it('Given white piece on A2 and black piece on A4, should move white piece to A3 and return expected list of events', () => {
+    const engine = new ChessEngine();
+    const whitePiece = new Pawn(Side.WHITE);
+    const blackPiece = new Pawn(Side.BLACK);
+    const boardWithPieces: SquareWithPiece = { A2: whitePiece, A4: blackPiece };
+    const chessBoard = new ChessBoard(boardWithPieces);
+    const player = new Player(Side.WHITE);
+    const squareFrom: Square = { column: 'A', row: 2 };
+    const squareTo: Square = { column: 'A', row: 3 };
+
+    const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo);
+
+    expect(chessBoard.onPositionPiece({ column: 'A', row: 2 })).toBe(undefined);
+    expect(chessBoard.onPositionPiece({ column: 'A', row: 3 })).toMatchObject(whitePiece);
+    expect(eventsList).toContain({ eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo });
+  });
+});
