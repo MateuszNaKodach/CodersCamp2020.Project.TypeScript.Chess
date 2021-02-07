dc7a8e34fcb99924e4d8ed4310a09570ca8a76f1
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-07 21:28:11 +0100

    #32 Add another test case

--- test/app/model/ChessEngine.spec.ts
+++ test/app/model/ChessEngine.spec.ts
@@ -4,6 +4,7 @@ import { ChessBoard } from '../../../src/app/model/ChessBoard';
 import { Pawn } from '../../../src/app/model/Pawn';
 import { Player } from '../../../src/app/model/Player';
 import 'jest-extended';
+import { Queen } from '../../../src/app/model/Queen';
 
 describe('Chess Engine', () => {
   it('Given white piece on A2 and black piece on A4, should move white piece to A3 and return expected list of events', () => {
@@ -19,5 +20,19 @@ describe('Chess Engine', () => {
     const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo, chessBoard);
 
     expect(eventsList).toIncludeSameMembers([{ eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo }]);
+  });
+  it('Given white piece on A2 and black piece on A4, should move white piece to A4 capturing oponents piece and return expected list of events', () => {
+    const engine = new ChessEngine();
+    const whitePiece = new Queen(Side.WHITE);
+    const blackPiece = new Pawn(Side.BLACK);
+    const boardWithPieces: SquareWithPiece = { A2: whitePiece, A4: blackPiece };
+    const chessBoard = new ChessBoard(boardWithPieces);
+    const player = new Player(Side.WHITE);
+    const squareFrom: Square = { column: 'A', row: 2 };
+    const squareTo: Square = { column: 'A', row: 4 };
+
+    const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo, chessBoard);
+
+    expect(eventsList).toIncludeSameMembers([{ eventType: 'PieceWasCaptured', piece: blackPiece, onSquare: squareTo }]);
   });
 });
