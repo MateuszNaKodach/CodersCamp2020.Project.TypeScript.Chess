03588b12a4a630011280886a7d1e0e33e1731f9d
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-07 23:10:39 +0100

    #32 Add move method signature in ChessModel interface

--- src/app/model/ChessModel.ts
+++ src/app/model/ChessModel.ts
@@ -1,1 +1,7 @@
-export interface ChessModel {}
+import { Player } from './Player';
+import { PieceWasCaptured, PieceWasMoved, Square } from './Types';
+import { ChessBoard } from './ChessBoard';
+
+export interface ChessModel {
+  move(byPlayer: Player, squareFrom: Square, squareTo: Square, board: ChessBoard): (PieceWasMoved | PieceWasCaptured)[];
+}
