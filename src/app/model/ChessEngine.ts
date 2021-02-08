c6868f816ecf4b34778f53f29c788cee1f4eccc2
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-08 18:08:49 +0100

    #32 Code refactor

--- src/app/model/ChessEngine.ts
+++ src/app/model/ChessEngine.ts
@@ -5,44 +5,53 @@ import { Player } from './Player';
 import { ChessBoard } from './ChessBoard';
 import { PieceWasMoved } from './PieceWasMoved';
 import { PieceWasCaptured } from './PieceWasCaptured';
+import { isDefined } from './HelperFunctions';
 
 export class ChessEngine implements ChessModel {
-  currentSide: Side = Side.BLACK;
+  private currentSide: Side = Side.BLACK;
 
   constructor(readonly board: ChessBoard) {}
 
   move(byPlayer: Player, squareFrom: Square, squareTo: Square): (PieceWasMoved | PieceWasCaptured)[] {
-    const chosenPiece = this.board.onPositionPiece(squareFrom) as Piece;
+    const chosenPiece = this.board.onPositionPiece(squareFrom);
+    if (!chosenPiece) {
+      throw new Error('There is no piece on this square.');
+    }
+    if (byPlayer.side == this.currentSide) {
+      throw new Error('Player can not move twice in a row.');
+    }
+    if (byPlayer.side !== chosenPiece.side) {
+      throw new Error('Player can not move other players pieces.');
+    }
     const possibleMoves = chosenPiece.possibleMoves(squareFrom, this.board);
-    const pieceOnSquare = this.board.onPositionPiece(squareTo);
-    const isOponent = pieceOnSquare && pieceOnSquare.side !== chosenPiece.side;
-
-    if (byPlayer.side == this.currentSide) throw new Error('Player can not move twice in a row.');
-    if (byPlayer.side !== chosenPiece.side) throw new Error('Player can not move other players pieces.');
-    if (
-      !possibleMoves.find((square: Square) => {
-        return square.column == squareTo.column && square.row == squareTo.row;
-      })
-    )
+    if (!this.canMoveOnSquare(possibleMoves, squareTo)) {
       throw new Error('Piece can not move to given square.');
-    const pieceWasMoved: PieceWasMoved = { eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo };
-    this.onPieceWasMoved(pieceWasMoved);
-    const pieceWasCaptured: PieceWasCaptured | undefined =
-      isOponent && isPiece(pieceOnSquare)
-        ? {
-            eventType: 'PieceWasCaptured',
-            piece: pieceOnSquare,
-            onSquare: squareTo,
-          }
-        : undefined;
+    }
 
+    const pieceWasMoved: PieceWasMoved = { eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo };
+    const pieceWasCaptured = this.pieceWasCaptured(squareTo, chosenPiece);
+
+    this.onPieceWasMoved(pieceWasMoved);
     return pieceWasCaptured ? [pieceWasMoved, pieceWasCaptured] : [pieceWasMoved];
+  }
+
+  private pieceWasCaptured(squareTo: Square, chosenPiece: Piece): PieceWasCaptured | undefined {
+    const pieceOnSquare = this.board.onPositionPiece(squareTo);
+    return pieceOnSquare?.isOpponentOf(chosenPiece) && isDefined(pieceOnSquare)
+      ? {
+          eventType: 'PieceWasCaptured',
+          piece: pieceOnSquare,
+          onSquare: squareTo,
+        }
+      : undefined;
   }
 
   private onPieceWasMoved(event: PieceWasMoved): void {
     this.board.movePiece(event.from, event.to);
     this.currentSide = event.piece.side;
   }
-}
 
-const isPiece = (varToCheck: unknown): varToCheck is Piece => (varToCheck as Piece) !== undefined;
+  private canMoveOnSquare(array: Square[], square: Square): boolean {
+    return array.some((el) => el.column === square.column && el.row === square.row);
+  }
+}
