07db06fd1f08367eec9264ae5fe8934529d33365
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-08 15:27:26 +0100

    #32 move refactor, tests refactor

--- src/app/model/ChessEngine.ts
+++ src/app/model/ChessEngine.ts
@@ -1,5 +1,5 @@
 import { ChessModel } from './ChessModel';
-import { Square } from './Types';
+import { Side, Square } from './Types';
 import { Piece } from './Piece';
 import { Player } from './Player';
 import { ChessBoard } from './ChessBoard';
@@ -7,11 +7,12 @@ import { PieceWasMoved } from './PieceWasMoved';
 import { PieceWasCaptured } from './PieceWasCaptured';
 
 export class ChessEngine implements ChessModel {
+  currentSide: Side = Side.WHITE;
+
   constructor(readonly board: ChessBoard) {}
 
   move(byPlayer: Player, squareFrom: Square, squareTo: Square): (PieceWasMoved | PieceWasCaptured)[] {
-    const moveEventsList: (PieceWasMoved | PieceWasCaptured)[] = [];
-    const chosenPiece = this.board.onPositionPiece(squareFrom) as Piece;
+    const chosenPiece = this.board.onPositionPiece(squareFrom) as Piece; // tutaj chyba też powinienem pozbyć się tego as Piece
     const possibleMoves = chosenPiece.possibleMoves(squareFrom, this.board);
     const pieceOnSquare = this.board.onPositionPiece(squareTo);
     const isOponent = pieceOnSquare && pieceOnSquare.side !== chosenPiece.side;
@@ -23,19 +24,26 @@ export class ChessEngine implements ChessModel {
       })
     )
       throw new Error('Piece can not move to given square.');
+    const pieceWasMoved: PieceWasMoved = { eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo };
+    this.onPieceWasMoved(pieceWasMoved);
+    const pieceWasCaptured: PieceWasCaptured | undefined =
+      isOponent && isPiece(pieceOnSquare)
+        ? {
+            //dodałem unię z undefined bo mi krzyczy
+            eventType: 'PieceWasCaptured',
+            piece: pieceOnSquare,
+            onSquare: squareTo,
+          }
+        : undefined;
 
-    this.board.movePiece(squareFrom, squareTo);
-    moveEventsList.push({ eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo });
-    if (isOponent && isPiece(pieceOnSquare))
-      moveEventsList.push({
-        eventType: 'PieceWasCaptured',
-        piece: pieceOnSquare,
-        onSquare: squareTo,
-      });
+    return pieceWasCaptured ? [pieceWasMoved, pieceWasCaptured] : [pieceWasMoved];
+  }
 
-    return moveEventsList;
+  private onPieceWasMoved(event: PieceWasMoved): void {
+    this.board.movePiece(event.from, event.to);
+    this.currentSide = event.piece.side;
   }
 }
 
-//Type Guard for Piece - wyczytałem, że można stworzyć taką funkcję 'pilnującą' albo możemy skorzystać z instanceof w warunku linia 30
+//Type Guard for Piece - wyczytałem, że można stworzyć taką funkcję 'pilnującą' albo możemy skorzystać z instanceof
 const isPiece = (varToCheck: unknown): varToCheck is Piece => (varToCheck as Piece) !== undefined;
--- test/app/model/ChessEngine.spec.ts
+++ test/app/model/ChessEngine.spec.ts
@@ -5,8 +5,6 @@ import { Pawn } from '../../../src/app/model/Pawn';
 import { Player } from '../../../src/app/model/Player';
 import 'jest-extended';
 import { Queen } from '../../../src/app/model/Queen';
-import { PieceWasMoved } from '../../../src/app/model/PieceWasMoved';
-import { PieceWasCaptured } from '../../../src/app/model/PieceWasCaptured';
 
 describe('Chess Engine', () => {
   it('Given white piece on A2 and black piece on A4, when move white piece from A2 to A3, then white piece was moved from A2 to A3', () => {
@@ -19,10 +17,18 @@ describe('Chess Engine', () => {
     const squareFrom: Square = { column: 'A', row: 2 };
     const squareTo: Square = { column: 'A', row: 3 };
 
-    const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo);
+    // engine.move(player, squareFrom, squareTo);
 
-    expect(eventsList).toIncludeSameMembers([{ eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo }]);
+    expect(engine.move(player, squareFrom, squareTo)).toIncludeSameMembers([
+      {
+        eventType: 'PieceWasMoved',
+        piece: whitePiece,
+        from: squareFrom,
+        to: squareTo,
+      },
+    ]);
   });
+
   it('Given white piece on A2 and black piece on A4, when move white piece from A2 to A4, then white piece was moved from A2 to A4 and piece from A4 was captured', () => {
     const whitePiece = new Queen(Side.WHITE);
     const blackPiece = new Pawn(Side.BLACK);
@@ -33,11 +39,49 @@ describe('Chess Engine', () => {
     const squareFrom: Square = { column: 'A', row: 2 };
     const squareTo: Square = { column: 'A', row: 4 };
 
-    const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo);
-
-    expect(eventsList).toIncludeSameMembers([
+    expect(engine.move(player, squareFrom, squareTo)).toIncludeSameMembers([
       { eventType: 'PieceWasCaptured', piece: blackPiece, onSquare: squareTo },
       { eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo },
     ]);
+  });
+
+  it('Should throw an Error if player wants to move oponents piece', () => {
+    const whitePiece = new Queen(Side.WHITE);
+    const blackPiece = new Pawn(Side.BLACK);
+    const boardWithPieces: SquareWithPiece = { A2: blackPiece, A4: whitePiece };
+    const chessBoard = new ChessBoard(boardWithPieces);
+    const engine = new ChessEngine(chessBoard);
+    const player = new Player(Side.WHITE);
+    const squareFrom: Square = { column: 'A', row: 2 };
+    const squareTo: Square = { column: 'A', row: 4 };
+
+    expect(() => engine.move(player, squareFrom, squareTo)).toThrowError('Player can not move other players pieces.');
+  });
+
+  it('Should throw an Error if player wants to move piece to not available square', () => {
+    const whitePiece = new Pawn(Side.WHITE);
+    const boardWithPieces: SquareWithPiece = { A2: whitePiece };
+    const chessBoard = new ChessBoard(boardWithPieces);
+    const engine = new ChessEngine(chessBoard);
+    const player = new Player(Side.WHITE);
+    const squareFrom: Square = { column: 'A', row: 2 };
+    const squareTo: Square = { column: 'A', row: 5 };
+
+    expect(() => engine.move(player, squareFrom, squareTo)).toThrowError('Piece can not move to given square.');
+  });
+
+  it('Should throw an Error if player wants to move twice', () => {
+    const whitePiece = new Pawn(Side.WHITE);
+    const boardWithPieces: SquareWithPiece = { A2: whitePiece };
+    const chessBoard = new ChessBoard(boardWithPieces);
+    const engine = new ChessEngine(chessBoard);
+    const player = new Player(Side.WHITE);
+    const squareStart: Square = { column: 'A', row: 2 };
+    const squareMiddle: Square = { column: 'A', row: 3 };
+    const squareFinish: Square = { column: 'A', row: 4 };
+
+    engine.move(player, squareStart, squareMiddle);
+
+    expect(() => engine.move(player, squareMiddle, squareFinish)).toThrowError();
   });
 });
