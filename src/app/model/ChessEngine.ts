8b45d3b74d787c30a307fc08a72c05e750c2072e
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-08 01:16:28 +0100

    #32 Correct imports in test file, refactor Chess Engine and add type guard for Piece

--- src/app/model/ChessEngine.ts
+++ src/app/model/ChessEngine.ts
@@ -1,37 +1,39 @@
 import { ChessModel } from './ChessModel';
-import { PieceMoves } from './PieceMoves';
-import { PiecePositions } from './PiecesPositions';
-import { PieceWasCaptured, PieceWasMoved, Square } from './Types';
+import { Square } from './Types';
 import { Piece } from './Piece';
 import { Player } from './Player';
 import { ChessBoard } from './ChessBoard';
+import { PieceWasMoved } from './PieceWasMoved';
+import { PieceWasCaptured } from './PieceWasCaptured';
 
-export class ChessEngine implements ChessModel, PieceMoves, PiecePositions {
+export class ChessEngine implements ChessModel {
   move(byPlayer: Player, squareFrom: Square, squareTo: Square, board: ChessBoard): (PieceWasMoved | PieceWasCaptured)[] {
     const moveEventsList: (PieceWasMoved | PieceWasCaptured)[] = [];
     const chosenPiece = board.onPositionPiece(squareFrom) as Piece;
     const possibleMoves = chosenPiece.possibleMoves(squareFrom, board);
-    const isOccupied = board.onPositionPiece(squareTo);
-    const isOponent = isOccupied && isOccupied?.side !== chosenPiece.side;
+    const pieceOnSquare = board.onPositionPiece(squareTo);
+    const isOponent = pieceOnSquare && pieceOnSquare.side !== chosenPiece.side;
 
-    if (byPlayer.side === chosenPiece.side) {
-      if (
-        possibleMoves.find((square: Square) => {
-          return square.column == squareTo.column && square.row == squareTo.row;
-        })
-      ) {
-        board.movePiece(squareFrom, squareTo);
-        if (isOponent) moveEventsList.push({ eventType: 'PieceWasCaptured', piece: isOccupied as Piece, onSquare: squareTo });
-        else moveEventsList.push({ eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo });
+    if (byPlayer.side !== chosenPiece.side) throw new Error('Player can not move other players pieces.');
+    if (
+      !possibleMoves.find((square: Square) => {
+        return square.column == squareTo.column && square.row == squareTo.row;
+      })
+    )
+      throw new Error('Piece can not move to given square.');
 
-        return moveEventsList;
-      } else throw new Error('Piece can not move to given square.');
-    } else throw new Error('Player can not move other players pieces.');
+    board.movePiece(squareFrom, squareTo);
+    moveEventsList.push({ eventType: 'PieceWasMoved', piece: chosenPiece, from: squareFrom, to: squareTo });
+    if (isOponent && isPiece(pieceOnSquare))
+      moveEventsList.push({
+        eventType: 'PieceWasCaptured',
+        piece: pieceOnSquare,
+        onSquare: squareTo,
+      });
+
+    return moveEventsList;
   }
-
-  onPositionPiece(square: Square): Piece | undefined {
-    return /*this.board[`${square.column}${square.row}`]*/ undefined;
-  }
-
-  movePiece(squareFrom: Square, squareTo: Square): void {}
 }
+
+//Type Guard for Piece
+const isPiece = (varToCheck: unknown): varToCheck is Piece => (varToCheck as Piece) !== undefined;
--- test/app/model/ChessEngine.spec.ts
+++ test/app/model/ChessEngine.spec.ts
@@ -1,13 +1,15 @@
 import { ChessEngine } from '../../../src/app/model/ChessEngine';
-import { PieceWasCaptured, PieceWasMoved, Side, Square, SquareWithPiece } from '../../../src/app/model/Types';
+import { Side, Square, SquareWithPiece } from '../../../src/app/model/Types';
 import { ChessBoard } from '../../../src/app/model/ChessBoard';
 import { Pawn } from '../../../src/app/model/Pawn';
 import { Player } from '../../../src/app/model/Player';
 import 'jest-extended';
 import { Queen } from '../../../src/app/model/Queen';
+import { PieceWasMoved } from '../../../src/app/model/PieceWasMoved';
+import { PieceWasCaptured } from '../../../src/app/model/PieceWasCaptured';
 
 describe('Chess Engine', () => {
-  it('Given white piece on A2 and black piece on A4, should move white piece to A3 and return expected list of events', () => {
+  it('Given white piece on A2 and black piece on A4, when move white piece from A2 to A3, then white piece was moved from A2 to A3', () => {
     const engine = new ChessEngine();
     const whitePiece = new Pawn(Side.WHITE);
     const blackPiece = new Pawn(Side.BLACK);
@@ -21,7 +23,7 @@ describe('Chess Engine', () => {
 
     expect(eventsList).toIncludeSameMembers([{ eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo }]);
   });
-  it('Given white piece on A2 and black piece on A4, should move white piece to A4 capturing oponents piece and return expected list of events', () => {
+  it('Given white piece on A2 and black piece on A4, when move white piece from A2 to A4, then white piece was moved from A2 to A4 and piece from A4 was captured', () => {
     const engine = new ChessEngine();
     const whitePiece = new Queen(Side.WHITE);
     const blackPiece = new Pawn(Side.BLACK);
@@ -33,6 +35,9 @@ describe('Chess Engine', () => {
 
     const eventsList: (PieceWasMoved | PieceWasCaptured)[] = engine.move(player, squareFrom, squareTo, chessBoard);
 
-    expect(eventsList).toIncludeSameMembers([{ eventType: 'PieceWasCaptured', piece: blackPiece, onSquare: squareTo }]);
+    expect(eventsList).toIncludeSameMembers([
+      { eventType: 'PieceWasCaptured', piece: blackPiece, onSquare: squareTo },
+      { eventType: 'PieceWasMoved', piece: whitePiece, from: squareFrom, to: squareTo },
+    ]);
   });
 });
